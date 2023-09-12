23522288bbffd5c8855719786bac18777e9eece7
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 20:10:19 +0600

    Add project files.

---
+++ Praktika_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Praktika_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое ФИО");
+            string a = Console.ReadLine();
+            Console.WriteLine("Приветсвую тебя - " + a);
+            
+        }
+    }
+}
